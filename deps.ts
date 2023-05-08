d789353ea68261ca06f9feb839a3b0877239c7ff
v1rtl <yo@v1rtl.site>
2023-05-08 12:52:48 +0300

    bump deps

--- deps.ts
+++ deps.ts
@@ -7,8 +7,8 @@ export type {
   Output,
   Wrapper,
 } from 'https://deno.land/x/solc@2.1.5/types.ts'
-export { createRequire } from 'https://deno.land/std@0.177.0/node/module.ts'
+export { createRequire } from 'node:module'
 export { Command } from 'https://deno.land/x/cliffy@v0.25.7/command/command.ts'
-export * as colors from 'https://deno.land/std@0.180.0/fmt/colors.ts'
-export { ensureDir } from 'https://deno.land/std@0.180.0/fs/ensure_dir.ts'
-export { walk } from 'https://deno.land/std@0.180.0/fs/walk.ts'
+export * as colors from 'https://deno.land/std@0.186.0/fmt/colors.ts'
+export { ensureDir } from 'https://deno.land/std@0.186.0/fs/ensure_dir.ts'
+export { walk } from 'https://deno.land/std@0.186.0/fs/walk.ts'
